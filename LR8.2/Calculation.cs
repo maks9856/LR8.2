6ff96dba9b6757cea9281ae32c0a9d7c1e693ae3
Max <marcenukm995@gmail.com>
2024-05-30 22:32:04 +0300

    доробив

--- LR8.2/Calculation.cs
+++ LR8.2/Calculation.cs
@@ -1,36 +1,30 @@
-﻿using OxyPlot;
+﻿using OxyPlot.Series;
+using OxyPlot;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Controls;
+using OxyPlot.Wpf;
 
 namespace LR8._2
 {
     public class Calculation
     {
-        public List<double> root { get; set; }
         public List<double> x { get; set; }
         public List<double> y { get; set; }
+        public List<double> root { get; set; }
         public string function { get; set; }
-        public List<double> calculatedy { get; set; }
+        public double sticky { get; set; }
+        public IModel bestModel { get; private set; }
 
-        public double[][] listSqurel;
-
-        public double sticky = 0;
         public Calculation(List<double> x, List<double> y)
         {
             this.x = x;
             this.y = y;
         }
 
-        public void Calc(DataGrid dataGrid)
+        public List<Result> Calc()
         {
+            List<Result> result = new List<Result>();
             GaussianMethod gaussianMethod = new GaussianMethod();
-            calculatedy = new List<double>();
-            root = new List<double>();
-
             List<IModel> models = new List<IModel>
             {
                 new Liner(),
@@ -41,48 +35,82 @@ namespace LR8._2
             };
 
             double minSticky = double.MaxValue;
-            IModel bestModel = null;
 
             foreach (var model in models)
             {
-                calculatedy.Clear();
-                listSqurel = model.Suma(x, y);
+                double[][] listSqurel = model.Suma(x, y);
                 root = gaussianMethod.Gauss(listSqurel);
                 function = model.StringFunction(root[0], root[1]);
+
+                List<double> calculatedy = new List<double>();
                 for (int i = 0; i < x.Count; i++)
                 {
                     calculatedy.Add(model.Function(root[0], root[1], x[i]));
                 }
 
-                double currentSticky = calculationSticky();
+                sticky = calculationSticky(y, calculatedy);
 
-                if (currentSticky < minSticky)
+                if (sticky < minSticky)
                 {
-                    minSticky = currentSticky;
+                    minSticky = sticky;
                     bestModel = model;
                 }
+                Result result1 = new Result();
+                result1.Root0 = root[0].ToString();
+                result1.Root1 = root[1].ToString();
+                result1.Function = function;
+                result1.Residual = sticky.ToString();
+                result.Add(result1);
             }
 
             if (bestModel != null)
             {
-                root = gaussianMethod.Gauss(bestModel.Suma(x, y));
+                double[][] listSqurel = bestModel.Suma(x, y);
+                root = gaussianMethod.Gauss(listSqurel);
                 function = bestModel.StringFunction(root[0], root[1]);
-                calculatedy.Clear();
-                for (int i = 0; i < x.Count; i++)
-                {
-                    calculatedy.Add(bestModel.Function(root[0], root[1], x[i]));
-                }   
             }
+            return  result;
         }
-
-        public double calculationSticky()
+        private double calculationSticky(List<double> y, List<double> calculatedy)
         {
-            sticky = 0;
+            double sticky = 0;
             for (int i = 0; i < x.Count; i++)
             {
-                sticky += Math.Pow(y[i] - calculatedy[i], 2);
+                sticky += Math.Pow(Math.Abs(y[i]) - Math.Abs(calculatedy[i]), 2);
             }
             return sticky;
         }
+
+        public void PlotBestModel(PlotModel model)
+        {
+            if (bestModel != null)
+            {
+                LineSeries lineSeries = new LineSeries
+                {
+                    Title = "Best Fit Model",
+                    Color = OxyColors.Blue,
+                    StrokeThickness = 2
+                };
+
+                //var scatterSeries = new ScatterSeries
+                //{
+                //    Title = "Точки",
+                //    MarkerType = MarkerType.Triangle,
+                //    MarkerSize = 4
+                //};
+
+                for (double xi = 0; xi <= 100000; xi += 0.5)
+                {
+                    double yi = bestModel.Function(root[0], root[1], xi);
+                    lineSeries.Points.Add(new DataPoint(xi, yi));
+                  
+                }
+
+                model.Series.Add(lineSeries);
+                
+            } 
+        }
+
+
     }
 }
---
+++ LR8.2/Result.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR8._2
+{
+    public class Result
+    {
+        public  string Root0 {  get; set; }
+        public string Root1 { get; set; }
+        public string Function { get; set; }
+        public string Residual { get; set; }
+
+    }
+}
